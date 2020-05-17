85d9b18c989ff3f93f80686ae40e0f98b6a1fea0
Vincent Toups <toups@unc.edu>
2020-05-17 14:12:27 -0400

    Added an additional alias for running r scripts.

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,4 @@
 alias m="docker run -v `pwd`:/host -it example make"
 alias b="docker run -v `pwd`:/host -it example /bin/bash"
 alias r="docker run -v `pwd`:/host -it example R"
+alias rs="docker run -v `pwd`:/host -it example Rscript"
